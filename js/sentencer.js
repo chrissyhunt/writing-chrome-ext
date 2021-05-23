f2d71d19eafb5cb4613b70009152fc435ab25b95
Chrissy Hunt <chrissy.hunt@gmail.com>
2021-05-23 12:04:10 -0400

    Add prompts

--- js/sentencer.js
+++ js/sentencer.js
@@ -10,6 +10,14 @@ const sillyPrompts = [
   "It was raining, and the {{ actor }} was sad.",
   "If she could just reach the {{ noun }}, it would be the best day EVER.",
   "{{ a_noun }}, {{ a_noun }}, {{ a_noun }}.",
+  "It was the {{ superlative }} of times, it was the {{ superlative }} of times.",
+  "Write about someone finally making their own {{ noun }}.",
+  "A character stands in front of two {{ nouns }}. What do they choose?",
+  "The {{ adjective }} {{ actor }} was sure someone was following them.",
+  "The {{ actor }} accepted the dare.",
+  "It was the {{ superlative }} {{ event }} she'd ever seen.",
+  "Set your story at {{ an_event }} that's gone horribly wrong.",
+  "Write about a character who thinks they're allergic to {{ nouns }}.",
 ];
 
 const seriousPrompts = [
@@ -59,6 +67,48 @@ const actors = [
   'frog that was secretly a prince',
 ];
 
+const superlatives = [
+  'best',
+  'worst',
+  'ugliest',
+  'most beautiful',
+  'loudest',
+  'quietest',
+  'longest',
+  'shortest',
+  'tastiest',
+  'most disgusting',
+  'most appalling',
+  'most enthralling',
+  'snappiest',
+  'drunkest',
+];
+
+const events = [
+  'birthday party',
+  'circus',
+  'opera',
+  'play',
+  'musical',
+  'Olympic race',
+  'political rally',
+  'wedding',
+  'bachelor party',
+  'casino night',
+  'office party',
+  'Christmas party',
+];
+
+function getEvent() {
+  const randomIndex = getRandomIndex(events.length);
+  return events[randomIndex];
+};
+
+function getSuperlative() {
+  const randomIndex = getRandomIndex(superlatives.length);
+  return superlatives[randomIndex];
+};
+
 function getEra() {
   const randomIndex = getRandomIndex(timePeriods.length);
   return timePeriods[randomIndex];
@@ -87,6 +137,11 @@ Sentencer.configure({
       return getOne(getActor);
     },
     era: getEra,
+    superlative: getSuperlative,
+    event: getEvent,
+    an_event: function() {
+      return getOne(getEvent);
+    },
   }
 });
 
